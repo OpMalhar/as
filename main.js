4ba3dabef74079bfe1874786b0a6a6fa2f252f71
OpMalhar <76428183+OpMalhar@users.noreply.github.com>
2021-05-28 14:59:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var d="Hrithik";
+var a="Roshan";
+var q=d.concat(a) ;
+console.log(a,d,q);
+var n1= 2;
+var n2=5;
+var add= n1+n2;
+var subtract= n1-n2;
+var divide= n1/n2;
+var multiply= n1*n2;
+console.log(n1,n2,add,subtract,divide,multiply);
